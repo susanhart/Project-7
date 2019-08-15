e21de88fa321255655730788859ca968a2948416
Sue <susanhartesquire@yahoo.com>
2019-08-15 19:33:22 -0400

    commit changes

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,1 +1,10 @@
 import React from 'react';
+
+// this will be used as a message for a url that was searched and does not exist
+const PageNotFound = () => (
+  <div >
+  <h2>Page Not Found</h2>
+  </div>
+);
+
+export default PageNotFound;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,23 @@
 import React, {Component} from 'react';
 
 export default class SearchForm extends Component {
-    
+ state = {
+     searchText: ''
+ }   
+ onsearchChange = e => {
+    this.setState({searchText: e.target.value});
+ }
+ handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSearch(this.userSearch.value);
+ }
+
+ render(){
+    return (
+        <form class='search-form' onSubmit={this.handleSubmit}>
+         <input type='search' onChange={this.onSearchChange} name='search' ref={(input) => this.userSearch = input} placeholder='search' />
+           <button type='submit'>Search</button>
+        </form>
+    );
+ }
 }
